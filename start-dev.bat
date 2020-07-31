d550f8a342587c6d65adfca2bb9f3120f7d1fa8d
Ali TEK <alitek@gmail.com>
2020-07-31 21:28:10 +0300

    start local test batch file changed

--- start-dev.bat
+++ start-dev.bat
@@ -1,2 +1,18 @@
+@echo off
 title API SYSTEM
-nodemon start-dev.js
+set screen=%1
+set position=%2
+set /a width=1600
+set /a height=900
+
+if %screen%==2 set width=1440
+if %screen%==2 set height=500
+
+echo w= %width%
+echo h= %height%
+
+REM c:\portable_programs\cmdow.exe @ /SIZ 700 400
+REM c:\portable_programs\cmdow.exe @ /MOV 1604 10
+REM c:\portable_programs\cmdow.exe @ /P
+echo program calisiyor
+pause
